71f1f666c11ae1338e06ec0c8622d3f05419f9dc
LinaSavova <45691615+LinaSavova@users.noreply.github.com>
2020-11-18 12:41:33 +0200

    Update WriteEmailTests.java

--- src/test/java/positive/WriteEmailTests.java
+++ src/test/java/positive/WriteEmailTests.java
@@ -14,41 +14,41 @@ public class WriteEmailTests extends BaseTests {
     @Test
     public void createEmail(){
         LogIn.goTo();
-        LogIn.logIn("barny1234", "Barny123456");
+        LogIn.logIn("putUsername", "putPassword");
         Write.createEmail();
         Write.verifyCreateNewEmail();
     }
     @Test
     public void sendEmail(){
         createEmail();
-        Write.sendEmail("kostata34@abv.bg", "Test", "This is a test email");
+        Write.sendEmail("putEmailOfTheReceiver", "Test", "This is a test email");
         Browser.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         Write.verifyEmailIsSent("Писмото беше изпратено успешно!", "Confirmation for sent message is not displayed");
     }
     @Test
     public void attachFile(){
         createEmail();
-        Write.uploadFile("kostata34@abv.bg", "Attachment Test", "Send email with attachment");
+        Write.uploadFile("putEmailOfTheReceiver", "Attachment Test", "Send email with attachment");
         Write.verifyEmailIsSent("Писмото беше изпратено успешно!", "Confirmation for sent message is not displayed");
     }
     //Това да го оправя, защото ми гърми на асърта - не разпознава "Черновата е записана успешно"
     @Test
     public void saveEmailInDrafts(){
         createEmail();
-        Write.fillInEmailFieldsRequired("smelichkata@abv.bg", "Test", "Testing save email in Drafts");
+        Write.fillInEmailFieldsRequired("putEmailOfTheReceiver, "Test", "Testing save email in Drafts");
         Write.saveDraft();
         Drafts.verifyDraftIsSaved("Черновата е записана успешно","Message for saved draft is not displayed");
     }
     @Test
     public void refuseEmailAccept(){
         createEmail();
-        Write.fillInEmailFieldsRequired("smelichkata@abv.bg", "Testing refuse email, OK", "Test");
+        Write.fillInEmailFieldsRequired("putEmailOfThe receiver", "Testing refuse email, OK", "Test");
         Write.refuseEmailCreatedOk();
     }
     @Test
     public void refuseEmailDismiss(){
         createEmail();
-        Write.fillInEmailFieldsRequired("smelichkata@abv.bg", "Testing refuse email, Cancel", "Teeeeeeeest");
+        Write.fillInEmailFieldsRequired("putEmailOfTheReceiver", "Testing refuse email, Cancel", "Teeeeeeeest");
         Write.refuseEmailCreatedCancel();
     }
 

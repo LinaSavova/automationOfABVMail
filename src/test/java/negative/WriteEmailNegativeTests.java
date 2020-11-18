0939b196bf51d20883b3beb2315745bdf7a27dbc
LinaSavova <45691615+LinaSavova@users.noreply.github.com>
2020-11-18 12:48:17 +0200

    Update WriteEmailNegativeTests.java

--- src/test/java/negative/WriteEmailNegativeTests.java
+++ src/test/java/negative/WriteEmailNegativeTests.java
@@ -13,26 +13,26 @@ public class WriteEmailNegativeTests extends BaseTests {
     @Test
     public void warnIfSubjectIsEmpty(){
         LogIn.goTo();
-        LogIn.logIn("barny1234", "Barny123456");
+        LogIn.logIn("putUsername", "putPassword");
         Write.createEmail();
-        Write.fillInToOnly("smelichkata@abv.bg");
+        Write.fillInToOnly("putEmailHere");
         Write.verifyWarningEmptySubject("Искате ли да изпратите писмото без тема?", "No warning displayed for empty subject");
     }
     @Test
     public void declineEmailIfSubjectIsEmpty(){
         LogIn.goTo();
-        LogIn.logIn("barny1234", "Barny123456");
+        LogIn.logIn("putUsername", "putPassword");
         Write.createEmail();
-        Write.fillInToOnly("smelichkata@abv.bg");
+        Write.fillInToOnly("putEmailHere");
         Write.verifyWarningEmptySubject("Искате ли да изпратите писмото без тема?", "No warning displayed for empty subject");
         Write.declineEmailWithoutSubject();
     }
     @Test
     public void sendEmailIfSubjectIsEmpty(){
         LogIn.goTo();
-        LogIn.logIn("barny1234", "Barny123456");
+        LogIn.logIn("putUsername", "putPassword");
         Write.createEmail();
-        Write.fillInToOnly("smelichkata@abv.bg");
+        Write.fillInToOnly("putEmailHere");
         Write.verifyWarningEmptySubject("Искате ли да изпратите писмото без тема?", "No warning displayed for empty subject");
         Write.confirmEmailWithoutSubject();
         Write.verifyEmailIsSent("Писмото беше изпратено успешно!", "Email not sent after confirmation");
@@ -40,7 +40,7 @@ public class WriteEmailNegativeTests extends BaseTests {
     @Test
     public void warnIfToIsEmpty(){
         LogIn.goTo();
-        LogIn.logIn("barny1234", "Barny123456");
+        LogIn.logIn("putUsername", "putPassword");
         Write.createEmail();
         Write.canNotSendEmailWithoutTo();
         Write.verifyWarningToEnterRecipient("Моля въведете поне един получател.", "No warning message for empty To field");
@@ -48,10 +48,10 @@ public class WriteEmailNegativeTests extends BaseTests {
     @Test
     public void warnIfInvalidReceiverEmailWritten(){
         LogIn.goTo();
-        LogIn.logIn("barny1234", "Barny123456");
+        LogIn.logIn("putUsername", "putPassword");
         Write.createEmail();
         Write.fillInSubjectOnly("test");
-        Write.fillInToOnly("smelichkataabv.bg");
+        Write.fillInToOnly("putEmailHere");
         Write.verifyWarningReceiverInvalidEmail("Писмото съдържа невалиден имейл адрес.", "Missing warning message when receiver's email is invalid");
     }
 

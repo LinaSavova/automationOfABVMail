b7fd5a03f76918e56577fa093e662200b2a82224
LinaSavova <45691615+LinaSavova@users.noreply.github.com>
2020-11-18 12:45:41 +0200

    Update SearchNegativeTests.java

--- src/test/java/negative/SearchNegativeTests.java
+++ src/test/java/negative/SearchNegativeTests.java
@@ -11,7 +11,7 @@ public class SearchNegativeTests extends BaseTests {
     @Test
     public void searchNoItemsFound(){
         LogIn.goTo();
-        LogIn.logIn("smelichkata", "smelichkata123456");
+        LogIn.logIn("putUsername", "putPassword");
         Search.searchForEmails("barny0");
         Home.verifyNoEmailsFound("Няма намерени писма по зададените критерии.", "The message for no results found is missing");
     }

fdb14e795f27f15f29ee5eda53aecea71239d144
LinaSavova <45691615+LinaSavova@users.noreply.github.com>
2020-11-18 12:45:06 +0200

    Update ContactsNegativeTests.java

--- src/test/java/negative/ContactsNegativeTests.java
+++ src/test/java/negative/ContactsNegativeTests.java
@@ -10,14 +10,14 @@ public class ContactsNegativeTests extends BaseTests {
     @Test
     public void contactsEmptyFolder(){
         LogIn.goTo();
-        LogIn.logIn("smelichkata", "smelichkata123456");
+        LogIn.logIn("putUsername", "putPassword");
         ABVPost.goToContacts();
         ABVPost.verifyEmptyContactsMessage("Групата е празна", "Not a warning message displayed");
     }
     @Test
     public void contactNotAddedTwice(){
         LogIn.goTo();
-        LogIn.logIn("barny1234", "Barny123456");
+        LogIn.logIn("putUsername", "putPassword");
         ABVPost.canNotAddTwice(0, "Counter still increasing");
     }
 }

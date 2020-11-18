33beca4802593fd51b658e29efbd00bc4129b499
LinaSavova <45691615+LinaSavova@users.noreply.github.com>
2020-11-18 12:33:34 +0200

    Update ContactsTests.java

--- src/test/java/positive/ContactsTests.java
+++ src/test/java/positive/ContactsTests.java
@@ -15,21 +15,21 @@ public class ContactsTests extends BaseTests {
     @Test
     public void loadContacts(){
         LogIn.goTo();
-        LogIn.logIn("barny1234", "Barny123456");
+        LogIn.logIn("putUsername", "putPassword");
         ABVPost.goToContacts();
         ABVPost.verifyContactsLoaded("No row with contacts is displayed");
     }
     @Test
     public void addToContacts(){
         LogIn.goTo();
-        LogIn.logIn("kostata34", "aA123456$");
+        LogIn.logIn("putUsername", "putPassword");
         ABVPost.addToContacts(0, "Counter not changing after added a new one");
         //Test verification is into the method itself
     }
     @Test
     public void deleteContact(){
         LogIn.goTo();
-        LogIn.logIn("kostata34", "aA123456$");
+        LogIn.logIn("putUsername", "putPassword");
         ABVPost.goToContacts();
         ABVPost.deleteContact(0, "Contacts' number is not decreased after deletion");
         //Test verification is into the method itself
@@ -37,13 +37,13 @@ public class ContactsTests extends BaseTests {
     @Test
     public void searchForContact(){
         LogIn.goTo();
-        LogIn.logIn("barny1234", "Barny123456");
+        LogIn.logIn("putUsername", "putPassword");
         Home.searchForContact();
         Home.logOut();
         LogIn.goTo();
         Browser.driver.switchTo().frame("abv-GDPR-frame").manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         Browser.driver.switchTo().defaultContent();
-        LogIn.logIn("smelichkata", "smelichkata123456");
+        LogIn.logIn("putSecondUsername", "putPassword");
         Home.goToInbox();
         Inbox.verifyInvitationReceived("Barny Rubble ви изпрати покана за чат", "Invitation not received");
     }
